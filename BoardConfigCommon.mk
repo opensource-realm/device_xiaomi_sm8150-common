64df2c3ec01b7d20419a68e002bd4eef750a5ebf
momenabdulrazek <momen.abdelrazek33@gmail.com>
2024-10-03 05:01:14 +0000

    Correct partitions implementation for ext4 file system

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -132,12 +132,12 @@ endif # TARGET_IS_VAB
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
 BOARD_USES_METADATA_PARTITION := true
 
-ALL_PARTITIONS := odm product system system_ext vendor
+SSI_PARTITIONS := product system system_ext
+TREBLE_PARTITIONS := odm vendor
+ALL_PARTITIONS := $(SSI_PARTITIONS) $(TREBLE_PARTITIONS)
 
 $(foreach p, $(call to-upper, $(ALL_PARTITIONS)), \
-    $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := ext4))
-
-$(foreach p, $(call to-upper, $(ALL_PARTITIONS)), \
+    $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := ext4) \
     $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
 
 # Partitions - dynamic
